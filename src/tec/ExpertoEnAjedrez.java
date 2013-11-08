71483f6df69b418c4b8727564784075711fa5ce2
5up3rD4n1 <daniel.soliscr@gmail.com>
2013-11-07 20:36:22 -0600

    Tablero logico lleno para usar

--- src/tec/ExpertoEnAjedrez.java
+++ src/tec/ExpertoEnAjedrez.java
@@ -1,6 +1,37 @@
 package tec;
 
 public class ExpertoEnAjedrez {
+	static private Pieza tLogico[][];
+	public ExpertoEnAjedrez(){
+		Pieza peonBlanco=new Pieza("blanca","peon","/img/peonBlanco.png");
+		Pieza peonNegro=new Pieza("negra","peon","/img/peonNegro.png");
+		
+		tLogico = new Pieza[8][8];
+		
+		for (int i=0; i<8;i++){
+				tLogico[1][i]= (Pieza) peonNegro;
+				tLogico[6][i]=peonBlanco;
+			}
+		tLogico[0][0].definePieza("negra","torre", "/img/torreNegro.png");
+		tLogico[0][7].definePieza("negra","torre", "/img/torreNegro.png");
+		tLogico[7][0].definePieza("blanca","torre", "/img/torreBlanco.png");
+		tLogico[7][7].definePieza("blanca","torre", "/img/torreBlanco.png");
+		tLogico[0][1].definePieza("negro","caballo", "/img/caballoNegro.png");
+		tLogico[0][6].definePieza("negro","caballo", "/img/caballoNegro.png");
+		tLogico[7][1].definePieza("blanca","caballo", "/img/caballoBlanco.png");
+		tLogico[7][6].definePieza("blanca","caballo", "/img/caballoBlanco.png");
+		tLogico[0][2].definePieza("negro","alfil", "/img/alfilNegro.png");
+		tLogico[0][5].definePieza("negro","alfil", "/img/alfilNegro.png");
+		tLogico[7][2].definePieza("blanco","alfil", "/img/alfilBlanco.png");
+		tLogico[7][5].definePieza("blanco","alfil", "/img/alfilBlanco.png");
+		tLogico[0][3].definePieza("negro","reina", "/img/reinaNegro.png");
+		tLogico[0][4].definePieza("negro","rey", "/img/reyNegro.png");
+		tLogico[7][4].definePieza("blanco","rey", "/img/reyBlanco.png");
+		tLogico[7][3].definePieza("blanco","reina", "/img/reinaBlanco.png");
+		
+	}
+	
+	
 
 	/*
 	 * 
