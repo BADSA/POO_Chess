90fc962c6b0b4b6a029529e5543acd287e04f5ae
5up3rD4n1 <daniel.soliscr@gmail.com>
2013-11-25 15:20:26 -0600

    Validacion del peon

--- src/cr/tec/programada/poo/modelo/ExpertoEnAjedrez.java
+++ src/cr/tec/programada/poo/modelo/ExpertoEnAjedrez.java
@@ -7,16 +7,17 @@ public class ExpertoEnAjedrez {
 	}
 	
     public boolean validarMovimiento(int x1,int y1,int x2,int y2){
-    
     	String tipo= tLogico.getTTipoPieza(x1, y1);
     	String color= tLogico.getTColorPieza(x1, y1);
+    	System.out.println(color+" "+tipo);
         if (tipo == "peon"){
             if (color == "negro"){
-                if (y1 < y2){
-                    if ((y1 == 1) && (y2 - y1 == 2) && (x2 == x1)){
+                if (x1 < x2){
+                    if ((x1 == 1) && (x2 - x1 == 2) && (y2 == y1)){
                         return true;
                     }
-                    if (y2 - y1 == 1 && Math.abs(x1 - x2) <= 1){
+                    if (x2 - x1 == 1 && Math.abs(y1 - y2) <= 1){
+                    	System.out.println("si");
                     	if (color==tLogico.getTColorPieza(x2, y2)){
                     		return false;
                     	}
@@ -25,11 +26,12 @@ public class ExpertoEnAjedrez {
                 }
 			}
             else if (color == "blanco"){
-                if (y1 > y2){
-                    if ((y1 == 6) && (y1 - y2 == 2) && (x2 == x1)){
+                if (x1 > x2){
+                    if ((x1 == 6) && (x1 - x2 == 2) && (y2 == y1)){
                         return true;
                     }
-                    if (y1 - y2 == 1 && Math.abs(x1 - x2) <= 1){
+                    if (x1 - x2 == 1 && Math.abs(y1 - y2) <= 1){
+                    	System.out.println(tLogico.getTColorPieza(x2, y2));
                     	if (color==tLogico.getTColorPieza(x2, y2)){
                     		return false;
                     	}		
@@ -86,6 +88,7 @@ public class ExpertoEnAjedrez {
     }
     if (tipo == "caballo"){
 	        if ((Math.abs(x1 - x2) + Math.abs(y1 - y2) == 3) && (Math.abs(x1-x2) >= 1) && (Math.abs(x1-x2) >= 1)){
+	        	System.out.println(tLogico.getTColorPieza(x2, y2));
 	            if (tLogico.getTColorPieza(x2, y2)==color){
 	            	return false;
 	            }
@@ -161,7 +164,7 @@ public class ExpertoEnAjedrez {
 		}
 		return true;
 	}
-    
+  /*  
     /*
 	 * 
 	// metodo Validar
@@ -203,7 +206,7 @@ public class ExpertoEnAjedrez {
     return false;
 }
     
-    */
+
 	
 	
 	//metodo Jaque
@@ -316,7 +319,6 @@ public class ExpertoEnAjedrez {
         }
 		return true;
     }
-
-
+*/
     
 }
